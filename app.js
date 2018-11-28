b640d33bfeacd667c20b5b35b647af5f71bc348c
Carlos Chacin <chacin.carlos9798@gmail.com>
2018-11-28 11:07:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,8 @@ io.on('connection', function(socket){
     socket.on('remoteCommand', function(button){
       // Create command
       console.log(button + " was pressed")
-      var command = "python ./tv_commands/ir_remote.py -p -g13 -f ./tv_commands/codes.json " + button;
+      var command = "python " + __dirname+"/tv_commands/ir_remote.py -p -g13 -f " + __dirname+"/tv_commands/codes.json " + button;
+      console.log(command);
       // Execute command
       exec(command, function(error, stdout, stderr) {});
     });
